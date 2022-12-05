bc2f4a8c5eb904675ae0afe7eb18fd0f6d0264da
eJwtzMGKwjAUheF3ybqLapoasx2YhSjOYhhhNuG2uamhaRNuWquI726Q7g4Hvv/JoG0xpd/Q48gUK0tRVwL2ssKaY7WzlgsJxkjLK8st1rJsNrbcsYJFCjdnkFb6XEt6WlPdNeivbrrDfDp/X+SJ4+G2FcvxKsSfkIf/5rK1w/hzzKkP0dMjYnYNAiHlN7Xhc4AZ3Kji3HjX6h4fBSEYFajLI4ZiTkgKB3C+WAL11oclY7xHR5g0TEyNs/ev1xshNk+ <mhjony336@gmail.com>
2022-12-05 20:37:12 +0600

    edit home page

--- app.js
+++ app.js
@@ -33,28 +33,45 @@ ball.addEventListener("click", () => {
 });
 
 
-var divA = document.getElementById('SearchIcon');
-var divB = document.getElementById('searchbar');
 
-divA.addEventListener("mouseenter", function(event) {
-  divB.style.display = 'block';
+function toggleClock() {
+  var my = document.getElementById('searchbar');
+  var displaySetting = my.style.display;
+  var Button = document.getElementById('SearchIcon');
+  if (displaySetting == 'block') {
+    my.style.display = 'none';
+  }
+  else {
+    my.style.display = 'block';
+  }
+}
+
+
+
+
+
+// var divA = document.getElementById('SearchIcon');
+// var divB = document.getElementById('searchbar');
+
+// divA.addEventListener("mouseenter", function(event) {
+//   divB.style.display = 'block';
 
  
-});
+// });
 
 
 
-divB.addEventListener("mouseenter", function(event) {
-  divB.style.display = 'block';
+// divB.addEventListener("mouseenter", function(event) {
+//   divB.style.display = 'block';
  
-});
+// });
 
-divB.addEventListener("mouseout", function(event) {
+// divB.addEventListener("mouseout", function(event) {
       
-  setTimeout(function() {
-    divB.style.display = 'none';
- }, 5000);
-});
+//   setTimeout(function() {
+//     divB.style.display = 'none';
+//  }, 5000);
+// });
 
 
 
@@ -112,7 +129,7 @@ function logincloseForm() {
 function signupopenForm() {
   document.getElementById("signupmyForm").style.display = "block";
   document.getElementById("myForm").style.display = "none";
-  document.getElementById("loginmyForm").style.display = "none";
+  document.getElementById("loginmyForm").style.display ="none";
 }
 
 function signupcloseForm() {
@@ -143,5 +160,4 @@ function showConfirmPassword() {
     x.type = "password";
   }
 }
-
 
