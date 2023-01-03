a2be60bb4ea88018f990efcd337dc5c8fb7b5163
foyez-hub <emamhasan9203@gmail.com>
2023-01-03 14:54:20 +0600

    de

--- sidenav.php
+++
@@ -1,9 +0,0 @@
-<div class="sidebar">
-        <!-- <div class="left-menu-icon fas fa-search" style="margin-top: 8px; " onclick="toggleClock()" id="SearchIcon">
-          -->       
-       <!--  </div> -->
-        <a style="margin-left: 20px;" href="indexlogin.php"><i class="left-menu-icon fas fa-home"></i></a>
-        <a style="margin-left: 20px;" href="watchParty.php"> <i class="left-menu-icon fas fa-users"></i></a>
-        <a style="margin-left: 20px;" href="allMoviesList.php"><i class="left-menu-icon fas fa-tv"></i></a>
-        <a style="margin-left: 20px;" href="meme.php"><i class="left-menu-icon fas fa-grin-alt"></i></a>
-    </div>
